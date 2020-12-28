416c3098bd4f34aa47ecb6863f0c3825a31e9579
mukhsmu <54389877+mukhsmu@users.noreply.github.com>
2020-12-28 17:26:27 -0400

    Update reacParams.h

--- include/reacParams.h
+++ include/reacParams.h
@@ -24,6 +24,7 @@ class reacParams : public TObject {
 		std::string d;
 		std::string e;
 		std::string f;
+		std::string foil;
 
 		Double_t E; // Beam energy
 		Double_t R1; // Resonance energy
@@ -33,6 +34,7 @@ class reacParams : public TObject {
 		Double_t SHAPE; // Resonance shape 0=Breit-Wigner, 1=Gaussian, 2=Square
 		
 		Bool_t SHT; // Reaction in solid Hydrogen/Deuterium target?
+		
 		
 		//virtual void ReadCalibPar(char* line);
 		virtual void ReadParams(char* line);
